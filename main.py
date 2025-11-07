882d140b5b72568cdbecadae062b8c6ab2850d66
Sanchjeev <sanchjeev.karikalan@imt-atlantique.net>
2025-11-07 11:22:01 +0100

    Modificiation train main

--- main.py
+++ main.py
@@ -12,36 +12,43 @@ Unet.load_state_dict(torch.load("model_weights.pth", map_location=torch.device('
 
 
 # Charger le fichier .nii.gz
-img = nib.load("data/MR-dataset/40-T1DUALin-src.nii.gz")
+img = nib.load("data/MR-dataset/39-T1DUALin-src.nii.gz")
 input= img.get_fdata()
 
-img = nib.load("data/MR-dataset/40-T1DUALout-src.nii.gz")
+img = nib.load("data/MR-dataset/39-T1DUAL-mask.nii.gz")
 output = img.get_fdata()
+
+couche = 15
 
 input1 = torch.tensor(input, dtype=torch.float32)
 input1 = input1.permute(2, 0, 1)
+input1 = input1[couche,:,:]
+output = torch.tensor(output, dtype=torch.float32)
+output = output.permute(2, 0, 1)
+output = output[couche,:,:]
 input1 = (input1.unsqueeze(0)).unsqueeze(0)
-input1 = input1[:,:,1,:,:]
 print(input1.shape)
 output_hat = Unet(input1)
 print(output_hat.shape)
-input1 = input1.permute(0, 2, 3, 1)
-output_hat = output_hat.permute(0, 2, 3, 1)
+output_hat = output_hat.squeeze()
 output_hat = output_hat.detach().numpy()
+input1 = input1.squeeze()
+
 print(input1.shape)
 print(output_hat.shape)
+print(output.shape)
 
 
 fig, axes = plt.subplots(1, 3, figsize=(15, 5))
 
 
-axes[0].imshow(input1[0,:, :, input1.shape[3] // 2], cmap="gray")
+axes[0].imshow(input1[:, :], cmap="gray")
 axes[0].set_title("in")
 
-axes[1].imshow(output_hat[0,:, :, output_hat.shape[3] // 2], cmap="gray")
+axes[1].imshow(output_hat[:, :], cmap="gray")
 axes[1].set_title("Unet(input)")
 
-axes[2].imshow(output[:, :, output.shape[2] // 2], cmap="gray")
+axes[2].imshow(output[:, :], cmap="gray")
 axes[2].set_title("out")
 
 for ax in axes: ax.axis("off")
--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import torch
 from torch import nn
 from sklearn.model_selection import train_test_split
 from U_Net import U_Net
+from losses import calc_loss
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -13,23 +14,25 @@ X = []
 y = []
 for i in range(40):
     data = (nib.load(f"data/MR-dataset/{i+1:02d}-T1DUALin-src.nii.gz")).get_fdata()
-    data2 = (nib.load(f"data/MR-dataset/{i+1:02d}-T1DUALout-src.nii.gz")).get_fdata()
-    for j in range(data.shape[2]):
-        X.append(data[:,:,j])
-        y.append(data2[:,:,j])
-
+    try:
+        data2 = (nib.load(f"data/MR-dataset/{i+1:02d}-T1DUAL-mask.nii.gz")).get_fdata()
+        for j in range(data.shape[2]):
+            X.append(data[:,:,j])
+            y.append(data2[:,:,j])
+    except:
+        pass
 
 def normalize(img, h,w):
     if img.shape[0]>h:
         start_h = (img.shape[0] - h)//2
         start_w = (img.shape[1] - w)//2
-        return img[start_h:start_h+h,start_w:start_w+w]
+        return img[None,start_h:start_h+h,start_w:start_w+w]
     else :
         start_h = (h-img.shape[0])//2
         start_w = (w-img.shape[1])//2
         pad = np.zeros((h,w))
         pad[start_h:start_h+img.shape[0],start_w:start_w+img.shape[1]] = img
-        return pad
+        return pad[None,:,:]
 
 for i in range(len(X)):
     X[i] = normalize(X[i],288,288)
@@ -47,34 +50,32 @@ X_train_tensor = torch.tensor(X_train)
 y_train_tensor = torch.tensor(y_train)
 
 train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
-train_loader  = DataLoader(train_dataset, batch_size=10, shuffle=True)
+train_loader  = DataLoader(train_dataset, batch_size=4, shuffle=True)
 
 
 
 model = U_Net(1,1)
 model.to(device)
 
-optimizer = torch.optim.SGD(model.parameters(),lr=0.01)
-criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(),lr=1e-4)
+criterion = calc_loss
 
 num_batches = len(train_loader)
-num_epochs = 10
+num_epochs = 1
 
 for epoch in range(num_epochs):
     for i, (batch_X, batch_y) in enumerate(train_loader, 1):
 
-        batch_X = batch_X[:, None, :, :] 
-
+     
         batch_X = batch_X.to(device) 
         batch_y = batch_y.to(device)
 
         outputs = model(batch_X)
-        outputs = outputs.squeeze()
-        
         loss = criterion(outputs, batch_y)
+        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        optimizer.zero_grad()
+        
 
         # Calcul du pourcentage
         percent_done = (i / num_batches) * 100
